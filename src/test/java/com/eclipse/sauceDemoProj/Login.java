3ee8c86908c4e89ae052494c83eb58976a6f22bb
charithabp <charithabp@AIPL8813LT20256.altimetrik.com>
2025-04-17 15:00:40 +0530

    Login page created

---
+++ src/test/java/com/eclipse/sauceDemoProj/Login.java
@@ -0,0 +1,19 @@
+package com.eclipse.sauceDemoProj;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Login {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
